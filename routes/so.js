fcb0be1fb8bbc4863a3c8f5b005a92f33edeefc6
999Kit <98326962+999Kit@users.noreply.github.com>
2023-11-10 15:21:13 +0700

    added some routes

--- routes/so.js
+++ routes/so.js
@@ -1,8 +1,22 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/qlQuan", (req, res) => {
-  res.render("so/qlqp/qlQuan.ejs");
+router.get("/qlquan", (req, res) => {
+  // Tìm lấy tất cả quận trong db
+  // const dsQuan = await Quan.find({})
+  const dsQuan = null;
+  res.render("so/qlqp/qlQuan", {dsQuan});
 });
+
+router.get("/qlquan/:quanId/qlphuong", (req, res) => {
+  // dựa vào id tìm quận tương ứng
+  // const quan = await Quan.findById(req.params.id);
+  const quan = null
+  res.render("so/qlqp/qlPhuong", {quan})
+})
+
+router.get("/qlquan/:quanId/edit", (req, res) => {
+  res.render("so/qlqp/editQuan")
+})
 
 export default router;
