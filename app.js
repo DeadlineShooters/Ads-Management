4522d75738e664a061afa99793231330a4a7ef40
Ngoc <dungtuyetngoc@gmail.com>
2023-11-11 18:00:40 +0700

    added login screen and login logic but still has login error

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ import phuongRoutes from "./routes/phuong.js";
 import quanRoutes from "./routes/quan.js";
 import soRoutes from "./routes/so.js";
 import ejsMate from "ejs-mate";
+import authRouter from "./routes/auth.js";
+import passport from "passport";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 const danApp = express();
 const canBoApp = express();
@@ -16,18 +20,36 @@ canBoApp.engine("ejs", ejsMate);
 canBoApp.set("view engine", "ejs");
 canBoApp.set("views", path.join(__dirname, "/views"));
 console.log(__dirname);
+canBoApp.use(passport.initialize());
 
 canBoApp.use(express.static("public"));
+canBoApp.use(
+  session({
+    secret: "keyboard cat",
+    saveUninitialized: false, // don't create session until something stored
+    resave: false, //don't save session if unmodified
+    store: MongoStore.create({
+      mongoUrl: "mongodb://127.0.0.1:27017/adsManagement",
+    }),
+  })
+);
+canBoApp.use(passport.authenticate("session"));
+
+// Initialize Passport
+
+// routes
 danApp.use("/", danRoutes);
 danApp.use("/public", express.static("public"));
 
+// Apply the isAuthenticated middleware to the root route
 canBoApp.get("/", (req, res) => {
-  res.render("dan/index.ejs"); // trang chủ của dân xài chung cho bên cán bộ
+  res.render("dan/index.ejs");
 });
 
 canBoApp.use("/phuong", phuongRoutes);
 canBoApp.use("/quan", quanRoutes);
 canBoApp.use("/so", soRoutes);
+canBoApp.use("/", authRouter);
 
 danApp.listen(3000, () => {
   console.log("Serving on port 3000");
---
+++ db.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+mongoose.connect("mongodb://127.0.0.1:27017/adsManagement");
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true, required: true },
+  hashed_password: { type: String, required: true },
+  salt: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+const hashedPassword = bcrypt.hashSync("letmein", salt);
+console.log("in db");
+
+// create an initial user (email: alice@gmail.com, password: letmein)
+const filter = { email: "alice@gmail.com" };
+const update = {
+  email: "alice@gmail.com",
+  hashed_password: hashedPassword,
+  salt: salt,
+};
+//  If no document is found, it can create a new document based on the update criteria.
+const user = await User.findOneAndUpdate(filter, update, {
+  new: true,
+  upsert: true,
+});
+
+export default User;
