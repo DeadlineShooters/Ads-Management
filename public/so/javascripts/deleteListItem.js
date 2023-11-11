7a7eb84bd616ae21986a79b0c132db61dd8e6f70
999Kit <98326962+999Kit@users.noreply.github.com>
2023-11-11 14:10:02 +0700

    tried adding delete button

--- public/so/javascripts/deleteListItem.js
+++ public/so/javascripts/deleteListItem.js
@@ -2,8 +2,32 @@
 // document.querySelectorAll('.bi-trash3-fill').forEach(item => {
 //     item.onclick = () => {
 //         if (confirm('Do you really want to remove this item?')) {
-//             item.parentElement.parentElement.parentElement.parentElement.remove();
-//             console.log(item.parentElement.parentElement.parentElement.parentElement)
+//             item.parentElement.parentElement.parentElement.parentElement.innerHTML= "";
+//             // console.log(item.parentElement.parentElement.parentElement.parentElement)
 //         }
 //     }
 // })
+// document.querySelectorAll('.ayo').forEach(item => {
+//     item.onclick = () => {
+//         if (confirm('Do you really want to remove this item?')) {
+//             item.remove();
+//             // console.log(item.parentElement.parentElement.parentElement.parentElement)
+//         }
+//     }
+// })
+
+document.addEventListener("DOMContentLoaded", function () {
+    // này xoá oke
+    // var elementToRemove = document.getElementById("myElement");
+    // elementToRemove.parentNode.removeChild(elementToRemove);
+
+    // này ko xoá dc :((
+    var trash = document.querySelectorAll('.btnayo');
+    trash.forEach((item) => {
+        item.addEventListener('click', () => {
+
+            var elementToRemove = document.getElementById("myElement");
+            elementToRemove.parentNode.removeChild(elementToRemove);
+        })
+    })
+});
