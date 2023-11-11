832886d30030ebf96b6123224e4d1a11c6a44951
999Kit <98326962+999Kit@users.noreply.github.com>
2023-11-11 12:24:45 +0700

    Added thêm/sửa phường quận

---
+++ public/so/javascripts/deleteListItem.js
@@ -0,0 +1,9 @@
+// không xài code này dc, muốn xoá phải dựa vào backend rồi frontend hiện lên thôi
+// document.querySelectorAll('.bi-trash3-fill').forEach(item => {
+//     item.onclick = () => {
+//         if (confirm('Do you really want to remove this item?')) {
+//             item.parentElement.parentElement.parentElement.parentElement.remove();
+//             console.log(item.parentElement.parentElement.parentElement.parentElement)
+//         }
+//     }
+// })
--- routes/so.js
+++ routes/so.js
@@ -8,6 +8,14 @@ router.get("/qlquan", (req, res) => {
   res.render("so/qlqp/qlQuan", {dsQuan});
 });
 
+router.get("/qlquan/new", (req, res) => {
+  res.render("so/qlqp/newQuan")
+})
+
+router.get("/qlquan/:quanId/edit", (req, res) => {
+  res.render("so/qlqp/editQuan")
+})
+
 router.get("/qlquan/:quanId/qlphuong", (req, res) => {
   // dựa vào id tìm quận tương ứng
   // const quan = await Quan.findById(req.params.id);
@@ -15,8 +23,12 @@ router.get("/qlquan/:quanId/qlphuong", (req, res) => {
   res.render("so/qlqp/qlPhuong", {quan})
 })
 
-router.get("/qlquan/:quanId/edit", (req, res) => {
-  res.render("so/qlqp/editQuan")
+router.get("/qlquan/:quanId/qlphuong/new", (req, res) => {
+  res.render("so/qlqp/newPhuong")
+})
+
+router.get("/qlquan/:quanId/qlphuong/:phuongID/edit", (req, res) => {
+  res.render("so/qlqp/editPhuong")
 })
 
 export default router;
