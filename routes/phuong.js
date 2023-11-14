ac77e6c0d5ab8f9ed7eb315e718f7370002d57bf
Ngoc <dungtuyetngoc@gmail.com>
2023-11-14 19:19:26 +0700

    finish screen, adding routing and error page

--- routes/phuong.js
+++ routes/phuong.js
@@ -1,6 +1,11 @@
 import express from "express";
 const router = express.Router();
 
+// địa chỉ cac diem quang cao
+const registeredAddresses = [
+  "157 Nguyễn Đình Chính, Phường 11, Quận Phú Nhuận",
+];
+
 router.get("/chi_tiet_diem", (req, res) => {
   // res.render("phuong/diemQC-details.ejs", { cssfile: "/phuong/style.css" });
   const diemQC = {
@@ -54,7 +59,14 @@ router.get("/chi-tiet-bao-cao", (req, res) => {
 
 router.get("/tao-yeu-cau-cap-phep", (req, res) => {
   const fullAddr = req.query.addr;
-  const parts = fullAddr.split(",");
+
+  const foundAddr = registeredAddresses.find((address) => address === fullAddr);
+
+  if (!foundAddr) {
+    res.render("error", { error: "404 Not Found" });
+    return;
+  }
+  const parts = foundAddr.split(",");
   res.render("phuong/taoYeuCauCapPhep", {
     addr: parts[0],
     ward: parts[1],
@@ -63,11 +75,21 @@ router.get("/tao-yeu-cau-cap-phep", (req, res) => {
   });
 });
 
+router.get("/error", (req, res) => {
+  res.render("error");
+});
 router.post("/update-report-status", (req, res) => {
   const status = req.body.status;
   const method = req.body.method;
   console.log(`Status: ${status}, Method: ${method}`);
   res.redirect("/chi-tiet-bao-cao");
+});
+
+router.post("/tao-yeu-cau-cap-phep", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  // route tới danh sách bảng quảng cáo
+  res.redirect("/bang-quang-cao");
 });
 
 /*------------ Quan ------------- */
