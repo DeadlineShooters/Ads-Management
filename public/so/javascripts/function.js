635556c7796c7eaa6b357a2aa0d6a1a5fc438aae
Ngoc <dungtuyetngoc@gmail.com>
2023-11-15 21:49:21 +0700

    cleaned up code and fixed error cant select quang cao on sidebar

--- public/so/javascripts/function.js
+++ public/so/javascripts/function.js
@@ -6,26 +6,6 @@ document.querySelectorAll(".bi-trash3-fill").forEach((item) => {
   });
 });
 
-// document.querySelectorAll('option').forEach((item, i) => {
-//     if (item.parentElement.selectedIndex == i) {
-//         item.selected = true;
-//     }
-// })
-
-// const select = document.querySelector('.ayyselect');
-// select.addEventListener("change", (e) => {
-//     window.location = select.options[select.selectedIndex].value;
-//     select.options[select.selectedIndex].selected = true;
-// })
-
-// document.querySelectorAll('.sidebar-nav').forEach(item => {
-//     item.onclick = (e) => {
-//         // alert('aa')
-//         item.classList.remove('active')
-//         item.classList.add('active')
-//     }
-// });
-
 // Tô màu cho button dc chọn ở sidebar
 document.addEventListener("DOMContentLoaded", function () {
   // Retrieve the last clicked link from localStorage
@@ -52,11 +32,15 @@ document.querySelectorAll(".sidebar-nav").forEach((item) => {
   item.onclick = () => {
     // Store the clicked link in localStorage
     localStorage.setItem("lastClickedLink", "#" + item.id);
-  };
-});
 
-// Hiện danh sách đang show ở dropdown select
-document.addEventListener("DOMContentLoaded", function () {
+    localStorage.setItem(
+      "lastSelectedOption",
+      'option[value="Các điểm đặt quảng cáo"]'
+    );
+  };
+
+  /* Hiện danh sách đang show ở dropdown select
+   */
   // Retrieve the last clicked link from localStorage
   const lastSelectedOption = localStorage.getItem("lastSelectedOption");
   // alert(lastSelectedOption);
@@ -73,6 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
 // Attach click event listeners to all links
 const select = document.querySelector("#qldropdown");
 if (select) {
