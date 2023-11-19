ceb0d7d0dbea6de1569bd27298d7a432f1266411
Ngoc <dungtuyetngoc@gmail.com>
2023-11-19 09:31:05 +0700

    added mongo name

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-await mongoose.connect("mongodb+srv://nhom09:atlas123@cluster0.hntnfkf.mongodb.net/?directConnection=true");
+await mongoose.connect("mongodb+srv://nhom09:atlas123@cluster0.hntnfkf.mongodb.net/Cluster0?directConnection=true");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
