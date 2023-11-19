2f26850951ac2ffed68d5142c301beb9ce70dc2d
Ngoc <dungtuyetngoc@gmail.com>
2023-11-19 08:30:06 +0700

    added missing

--- models/user.js
+++ models/user.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
-await mongoose.connect(
-  "mongodb+srv://nhom09:atlas123@cluster0.hntnfkf.mongodb.net/"
-);
+await mongoose.connect("mongodb+srv://nhom09:atlas123@cluster0.hntnfkf.mongodb.net/?directConnection=true");
 
 const userSchema = new mongoose.Schema({
-  username: {type: String, required: true},
+  username: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   hashed_password: { type: String, required: true },
   salt: { type: String, required: true },
@@ -13,7 +11,7 @@ const userSchema = new mongoose.Schema({
   district: { type: String },
   phone: { type: String },
   birthday: { type: Date },
-  daycreated: { type: Date, default: Date.now()}
+  daycreated: { type: Date, default: Date.now() },
 });
 
 const User = mongoose.model("User", userSchema);
