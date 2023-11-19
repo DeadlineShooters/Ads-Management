93ec7cbeb62f2fe3ad042be925871c0b74155220
999Kit <98326962+999Kit@users.noreply.github.com>
2023-11-19 11:32:06 +0700

    split app.js file

--- appCanbo.js
+++ appCanbo.js
@@ -104,10 +104,10 @@ canBoApp.use("/so/quanly", soQuanLyRoutes);
 canBoApp.use("/so/hanhchinh", soHanhChinhRoutes);
 canBoApp.use("/so/canbo", soCanBoRoutes);
 
-danApp.listen(3000, () => {
-  console.log("Serving on port 3000");
-});
-// canBoApp.listen(9000, () => {
-//   console.log("Serving on port 9000");
+// danApp.listen(3000, () => {
+//   console.log("Serving on port 3000");
 // });
+canBoApp.listen(9000, () => {
+  console.log("Serving on port 9000");
+});
 // testing revert
