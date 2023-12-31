e74cf392f12b4306dd1f21f1c827908b2e285d04
nmthong226 <nmthong226@gmail.com>
2024-01-01 01:48:35 +0700

    + update trang thong ke-baocao

--- controllers/so/hanhChinh/thongKeBaoCao.js
+++ controllers/so/hanhChinh/thongKeBaoCao.js
@@ -1,14 +1,54 @@
+import Ward from "../../../models/ward.js";
+import District from "../../../models/district.js";
+import Report from "../../../models/report.js";
+import ReportType from "../../../models/reportType.js";
+import mongoose from "mongoose";
+const { Types } = mongoose;
+
 export const tkBaoCaoXuLy = async (req, res) => {
-    const thongKe = [
+    try {
+        const sumReports = await Report.countDocuments();
+        const handleReports = await Report.find({ status: "Đang xử lý" }).countDocuments();
+        const doneReports = await Report.find({ status: "Đã xử lý xong" }).countDocuments();
+
+        const ToGiacReport = await ReportType.find({name: "Tố giác sai phạm"});
+        const TongToGiacReport = await Report.find({reportType: new Types.ObjectId(ToGiacReport[0]._id)}).countDocuments();
+
+        const DangKyReport = await ReportType.find({name: "Đăng ký nội dung"});
+        const TongDangKyReport = await Report.find({reportType: new Types.ObjectId(DangKyReport[0]._id)}).countDocuments();
+
+        const DongGopReport = await ReportType.find({name: "Đăng ký nội dung"});
+        const TongDongGopReport = await Report.find({reportType: new Types.ObjectId(DongGopReport[0]._id)}).countDocuments();
+
+        const GiaiDapReport = await ReportType.find({name: "Đăng ký nội dung"});
+        const TongGiaiDapReport = await Report.find({reportType: new Types.ObjectId(GiaiDapReport[0]._id)}).countDocuments();
+
+        const TongBaoCaoViPhamDiemQC = await Report.find({randomPoint: {$exists: true}}).countDocuments();
+        const TongBaoCaoViPhamBangQC = await Report.find({adBoard: {$exists: true}}).countDocuments();
+
+        const wardList = await Ward.find({}).populate({
+            path: "district", model: 'District'
+        });
+        const districtList = await District.find({});
+        const breadcrumbs = [];
+
+        res.render('so/hanhChinh/dsThongKeBaoCao.ejs', 
         {
-            quan: "Tân Bình",
-            phuong: "Phường 13",
-            soLuongBaoCao: 7,
-            dangXuLy: 4,
-            xuLyXong: 3,
-            danhGia: "43%",
-        }
-    ]
-    const breadcrumbs = [];
-    res.render('so/hanhChinh/dsThongKeBaoCao.ejs', {object: thongKe, breadcrumbs});
+            sumReports,
+            handleReports,
+            doneReports,
+            TongToGiacReport,
+            TongDangKyReport,
+            TongDongGopReport,
+            TongGiaiDapReport,
+            TongBaoCaoViPhamDiemQC,
+            TongBaoCaoViPhamBangQC,
+            breadcrumbs,
+            wardList,
+            districtList
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
 }
